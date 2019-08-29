16d021dadc2d0f067dd360400bfe787b4fb950e2
Rajesh <rajeshceg3@gmail.com>
2019-08-29 18:20:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () =>{
+    let long, lat;
+    if ( navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( (position) =>{            
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            let temperatureDegreeText = document.querySelector(".temperature-degree");
+            let descriptionText = document.querySelector(".description");
+            let locationText = document.querySelector(".location");
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/83223a78a86f77f0073c5a481897669a/${lat},${long}`;
+            fetch(api)
+            .then( function(response){
+                return response.json();
+            })
+            .then( function(data){
+                console.log(data);
+                const { temperature, summary} = data.currently;
+                temperatureDegreeText.textContent = temperature;
+                descriptionText.textContent = summary;
+                locationText.textContent = data.timezone;
+            })
+            .catch(function(error){
+                alert(error);
+            })
+        });
+} 
+});
